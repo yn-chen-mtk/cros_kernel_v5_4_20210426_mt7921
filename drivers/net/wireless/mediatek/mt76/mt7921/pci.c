79a216a1c0b01d946d94f67b4727c63580f60966
YN Chen <yn.chen@mediatek.com>
2021-05-04 10:11:32 +0800

    [Debug] set WiFi driver version to v2.2.0_20210503

--- drivers/net/wireless/mediatek/mt76/mt7921/pci.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/pci.c
@@ -12,7 +12,7 @@
 #include "mcu.h"
 #include "../trace.h"
 
-#define MT7921_DRIVER_VERSION "v2.1.1_20210428"
+#define MT7921_DRIVER_VERSION "v2.2.0_20210503"
 
 static const struct pci_device_id mt7921_pci_device_table[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x7961) },
