ba2c3cc209febe6838b7fb05e7a4addae0d10f26
YN Chen <yn.chen@mediatek.com>
2021-05-07 00:39:00 +0800

    [Fix] 2G TX improvement

--- drivers/net/wireless/mediatek/mt76/mt76_connac_mcu.c
+++ drivers/net/wireless/mediatek/mt76/mt76_connac_mcu.c
@@ -707,6 +707,12 @@ void mt76_connac_mcu_sta_tlv(struct mt76_phy *mphy, struct sk_buff *skb,
 	phy->basic_rate = cpu_to_le16((u16)vif->bss_conf.basic_rates);
 	phy->rcpi = rcpi;
 
+	if (sta->ht_cap.ht_supported) {
+		phy->ampdu =
+			FIELD_PREP(IEEE80211_HT_AMPDU_PARM_FACTOR, sta->ht_cap.ampdu_factor) |
+			FIELD_PREP(IEEE80211_HT_AMPDU_PARM_DENSITY, sta->ht_cap.ampdu_density);
+	}
+
 	tlv = mt76_connac_mcu_add_tlv(skb, STA_REC_RA, sizeof(*ra_info));
 	ra_info = (struct sta_rec_ra_info *)tlv;
 	ra_info->legacy = cpu_to_le16((u16)sta->supp_rates[band]);
